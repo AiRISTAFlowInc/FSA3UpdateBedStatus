25de8d15b41afc97ae1479685895d9a79974a048
christian <christian.heuchert@airista.com>
2023-09-20 09:42:31 -0400

    UpdateBedStatus

---
+++ activity.go
@@ -0,0 +1,296 @@
+package UpdateBedStatus
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	"github.com/project-flogo/core/activity"
+)
+
+func init() {
+	_ = activity.Register(&Activity{}) //activity.Register(&Activity{}, New) to create instances using factory method 'New'
+}
+
+var activityMd = activity.ToMetadata(&Input{}, &Output{})
+
+// Activity is an sample Activity that can be used as a base to create a custom activity
+type Activity struct {
+}
+
+// Metadata returns the activity's metadata
+func (a *Activity) Metadata() *activity.Metadata {
+	return activityMd
+}
+
+// Eval implements api.Activity.Eval - Logs the Message
+func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
+
+	input := &Input{}
+	err = ctx.GetInputObject(input)
+	if err != nil {
+		return true, err
+	}
+
+	updateStatus := UpdateBedStatus(input.IP, input.CustomerId, input.Username, input.Password, input.MAC)
+
+	output := &Output{Status: updateStatus}
+
+	fmt.Println("Output: ", output.Status)
+	ctx.Logger().Info("Output: ", output)
+
+	err = ctx.SetOutputObject(output)
+	if err != nil {
+		return true, err
+	}
+
+	return true, nil
+}
+
+func UpdateBedStatus(IP string, customerId string, uname string, pword string, MAC string) bool {
+	itemID := GetByMACAddress(IP, customerId, uname, pword, MAC)
+	staff := GetByStaffId(IP, customerId, uname, pword, itemID)
+	status := nextBedStatus(IP, customerId, uname, pword, itemID, staff)
+	return status
+}
+
+func GetByMACAddress(IP string, customerId string, uname string, pword string, MAC string) string {
+	// Create an HTTP client
+	client := &http.Client{}
+	cleanMAC := url.QueryEscape(MAC)
+
+	// Create the request
+	url := "http://" + IP + "/XpertRestApi/api/Device/GetByMacAddress?MacAddress="+cleanMAC+"&CustomerId="+customerId
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return ""
+	}
+
+	// Add basic authentication to the request header
+	auth := uname + ":" + pword
+	basicAuth := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+	req.Header.Add("Authorization", basicAuth)
+
+	// Perform the request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return ""
+	}
+	defer resp.Body.Close()
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return ""
+	}
+
+	// Unmarshal the config JSON into an object
+	var device Device
+	errUnmarshal := json.Unmarshal(body, &device)
+	if errUnmarshal != nil {
+	 	fmt.Println(errUnmarshal)
+		return ""
+	}
+
+	return strconv.Itoa(device.ItemID)
+}
+
+func GetByStaffId(IP string, customerId string, uname string, pword string, staffId string) Staff {
+	// Staff to return 
+	var staff Staff
+
+	// Create an HTTP client
+	client := &http.Client{}
+
+	// Create the request
+	url := "http://" + IP + "/XpertRestApi/api/Staff/GetByStaffId?StaffId="+staffId+"&CustomerId="+customerId
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return staff
+	}
+
+	// Add basic authentication to the request header
+	auth := uname + ":" + pword
+	basicAuth := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+	req.Header.Add("Authorization", basicAuth)
+
+	// Perform the request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return staff
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return staff
+	}
+
+	// Unmarshal the config JSON into an object
+	errUnmarshal := json.Unmarshal(body, &staff)
+	if errUnmarshal != nil {
+	 	fmt.Println(errUnmarshal)
+		return staff
+	}
+
+	return staff
+}
+
+func nextBedStatus(IP string, customerId string, uname string, pword string, staffId string, staff Staff) bool {
+	assocItemId := strconv.Itoa(staff.AssocItemID)
+	status:= false
+
+	switch bedStatus := staff.BedStatus; bedStatus{
+	case "ASSIGNED":
+		status = changeItemAssociation(IP, customerId, uname, pword, staffId, assocItemId, "DISCHARGING")
+	case "DISCHARGING":
+		endItemAssociation(IP, customerId, uname, pword, staffId, assocItemId)
+		status = createItemAssociation(IP, customerId, uname, pword, staffId, assocItemId, "CLEANING")
+	case "CLEANING":
+		status = endItemAssociation(IP, customerId, uname, pword, staffId, assocItemId)
+	}
+	
+	return status
+}
+
+func changeItemAssociation(IP string, customerId string, uname string, pword string, itemId string, assocItemId string, associationType string) bool{
+	// Create an HTTP client
+	client := &http.Client{}
+	// Create the request
+	url := "/api/Staff/ChangeItemAssociation?CustomerId="+customerId+"&AssociationType="+associationType+"&ItemID="+itemId+"&AssociatedItemID="+assocItemId
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return false
+	}
+
+	// Add basic authentication to the request header
+	auth := uname + ":" + pword
+	basicAuth := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+	req.Header.Add("Authorization", basicAuth)
+	// Perform the request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return false
+	}
+
+	// Unmarshal the config JSON into an object
+	var response Response
+	errUnmarshal := json.Unmarshal(body, &response)
+	if errUnmarshal != nil {
+	 	fmt.Println(errUnmarshal)
+		return false
+	}
+	if(response.ErrorMessage == ""){// If successful return true 
+		return true
+	}
+
+	return false
+}
+
+func endItemAssociation(IP string, customerId string, uname string, pword string, itemId string, assocItemId string) bool{
+	// Create an HTTP client
+	client := &http.Client{}
+	// Create the request
+	url := "/api/Staff/EndItemAssociation?CustomerId="+customerId+"&ItemID="+itemId+"&AssociatedItemID="+assocItemId
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return false
+	}
+
+	// Add basic authentication to the request header
+	auth := uname + ":" + pword
+	basicAuth := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+	req.Header.Add("Authorization", basicAuth)
+	// Perform the request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return false
+	}
+
+	// Unmarshal the config JSON into an object
+	var response Response
+	errUnmarshal := json.Unmarshal(body, &response)
+	if errUnmarshal != nil {
+	 	fmt.Println(errUnmarshal)
+		return false
+	}
+	if(response.ErrorMessage == ""){// If successful return true 
+		return true
+	}
+
+	return false
+}
+
+func createItemAssociation(IP string, customerId string, uname string, pword string, itemId string, assocItemId string, associationType string) bool{
+	// Create an HTTP client
+	client := &http.Client{}
+	// Create the request
+	url := "/api/Staff/CreateItemAssociation?CustomerId="+customerId+"&AssociationType="+associationType+"&ItemID="+itemId+"&AssociatedItemID="+assocItemId
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return false
+	}
+
+	// Add basic authentication to the request header
+	auth := uname + ":" + pword
+	basicAuth := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+	req.Header.Add("Authorization", basicAuth)
+	// Perform the request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return false
+	}
+
+	// Unmarshal the config JSON into an object
+	var response Response
+	errUnmarshal := json.Unmarshal(body, &response)
+	if errUnmarshal != nil {
+	 	fmt.Println(errUnmarshal)
+		return false
+	}
+	if(response.ErrorMessage == ""){// If successful return true 
+		return true
+	}
+
+	return false
+}
---
+++ metadata.go
@@ -0,0 +1,74 @@
+package UpdateBedStatus
+
+import (
+	"github.com/project-flogo/core/data/coerce"
+)
+
+type Input struct {
+	IP         string `md:"IP,required"`
+	CustomerId string `md:"CustomerId,required"`
+	Username   string `md:"Username,required"`
+	Password   string `md:"Password,required"`
+	MAC 	   string `md:"MAC,required"`
+}
+
+func (i *Input) FromMap(values map[string]interface{}) error {
+	strVal, _ := coerce.ToString(values["IP"])
+	i.IP = strVal
+
+	strVal, _ = coerce.ToString(values["CustomerId"])
+	i.CustomerId = strVal
+
+	strVal, _ = coerce.ToString(values["Username"])
+	i.Username = strVal
+
+	strVal, _ = coerce.ToString(values["Password"])
+	i.Password = strVal
+
+	strVal, _ = coerce.ToString(values["MAC"])
+	i.MAC = strVal
+	return nil
+}
+
+func (i *Input) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"IP":         i.IP,
+		"CustomerId": i.CustomerId,
+		"Username":   i.Username,
+		"Password":   i.Password,
+		"MAC": 	  i.MAC,
+	}
+}
+
+type Output struct {
+	Status bool `md:"Status"`
+}
+
+func (o *Output) FromMap(values map[string]interface{}) error {
+	boolStatus, _ := coerce.ToBool(values["Status"])
+	o.Status = boolStatus
+	return nil
+}
+
+func (o *Output) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"Status": o.Status,
+	}
+}
+
+type Response struct {
+	ElapsedTimeInMillseconds float64 `json:"ElapsedTimeInMillseconds"`
+	ErrorMessage             string  `json:"ErrorMessage"`
+	SuccessMessage           string  `json:"SuccessMessage"`
+	HasError                 bool    `json:"HasError"`
+	ID                       int     `json:"Id"`
+}
+
+type Device struct {
+	ItemID                   int       `json:"ItemId"`
+}
+
+type Staff struct {
+	BedStatus                string `json:"BedStatus"`
+	AssocItemID              int     `json:"AssocItemID"`
+}
